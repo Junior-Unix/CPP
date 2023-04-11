13025ccd8b2e86a464a6904b78a9598c97ebaa4d
Nelson Leme de Almeida Junior <sonjunior@live.com>
2023-04-11 20:40:27 -0300

    Treino.

---
+++ nuke/main.cxx
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "GradeBook.h"
+
+
+int main(){
+
+    GradeBook gradeBook1( "Construção do gradeBook1. ");
+    GradeBook gradeBook2( "Construção do gradeBook2. ");
+
+        cout 
+        << "Primeira " << gradeBook1.getCourseName()
+        << "Segunda " << gradeBook2.getCourseName()
+        << endl;
+    
+    
+    
+    return 0;
+}
