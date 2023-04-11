8920667fa128050bc8be084af481f5ae8de0fec7
Nelson Leme de Almeida Junior <sonjunior@live.com>
2023-04-11 10:53:50 -0300

    Protótipos de funções.

---
+++ nuke/main.cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "GradeBook.h"
+
+int main(){
+
+    GradeBook gradeBook1( "Construção do gradeBook1: ");
+    GradeBook gradeBook2( "Construção do gradeBook2: ");
+
+        cout
+            << "Primeiro texto: " << gradeBook1.getCourseName()
+            << "Segundo texto: " << gradeBook2.getCourseName();
+            << endl;
+
+
+
+    return 0;
+}
