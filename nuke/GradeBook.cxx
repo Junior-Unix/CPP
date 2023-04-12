fe0d32ac25424c14f5d08d99665b9ac72b012595
Nelson Leme de Almeida Junior <sonjunior@live.com>
2023-04-11 21:00:44 -0300

    Modificações GradeBook.cxx

---
+++ nuke/GradeBook.cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "GradeBook.h"
+
+GradeBook::GradeBook( string name ){
+    setCourseName( name );
+}
+void GradeBook::setCourseName( string name ){
+    courseName = name;
+}
+string GradeBook::getCourseName(){
+    return courseName;
+}
+void GradeBook::displayMessage(){
+    cout
+        << "Welcome! " << getCourseName()
+        << endl;
+}
