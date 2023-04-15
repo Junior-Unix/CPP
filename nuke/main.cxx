fc1d3837c4be40a64c0b4198171fe4bd7050fccb
Junior-Unix <sonjunior@live.com>
2023-04-15 17:58:11 -0300

    CLS.

--- nuke/main.cxx
+++ nuke/main.cxx
@@ -2,17 +2,9 @@
 using std::cout;
 using std::endl;
 
-#include "GradeBook.h"
-
 int main() {
 
-	GradeBook gradeBook1("Texto com mais de vinte e cinco letras.");
-	GradeBook gradeBook2("Menos de 25");
 
-	cout
-		<< "Primeiro" << gradeBook1.getCourseName()
-		<< "Segundo" << gradeBook2.getCourseName()
-		<< endl;
 
 	return 0;
 }
