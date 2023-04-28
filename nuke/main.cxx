bef8e522763db4cee283f8360e8aa685fbd4b2d1
Junior-Unix <sonjunior@live.com>
2023-04-27 22:05:25 -0300

    Treino no VIM, sucesso.

--- nuke/main.cxx
+++ nuke/main.cxx
@@ -1,11 +1,12 @@
 #include "GradeBook.h"
 
-int main() {
-
-	GradeBook myGradeBook("CS101 C++ Programing.");
+int main(){
+	
+	GradeBook myGradeBook("CS101 C++ Programing!");
 
 	myGradeBook.displayMessage();
 	myGradeBook.determineClassAverage();
 
 	return 0;
+
 }
