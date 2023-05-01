591eafe9521919a3ae9c73714b5b7ee52017e1c5
Junior-Unix <sonjunior@live.com>
2023-04-30 22:06:39 -0300

    Switch.

--- GradeBook.h
+++ GradeBook.h
@@ -2,19 +2,20 @@
 using std::string;
 
 class GradeBook{
-	public:
-		GradeBook(string);
-		void setCourseName(string);
-		string getCourseName();
-		void displayMessage();
-		void inputGrades();
-		void displayGradesReport();
-	private:
-		string courseName();
-		int aCount;
-		int bCount;
-		int cCount;
-		int dCount;
-		int eCount;
-		int fCount;
+private:
+	string courseName;
+	int aCount;
+	int bCount;
+	int cCount;
+	int dCount;
+	int eCount;
+	int fCount;
+public:
+	GradeBook(string);
+	void setCourseName(string);
+	string getCourseName();
+	void displayMessage();
+	void determineClassAverage();
+	void inputGrades();
+	void displayGradeReport();
 };
